c7ef89cdd94953899f5dad72ef8896b1d9f061da
Miguel Silva <miguelangelorsilva1@gmail.com>
2025-12-30 11:59:55 +0000

    Lucas e Miguel: - Alterações nos diagramas

--- src/types/GameScoreMetadata.ts
+++ src/types/GameScoreMetadata.ts
@@ -1,5 +1,3 @@
-import {GamePointsPerPlayerProps} from "@/src/types/GamePointsPerPlayerProps";
-
 export interface GameScoreMetadata {
     room_token: string;
     data: {name: string, points: number}[];
