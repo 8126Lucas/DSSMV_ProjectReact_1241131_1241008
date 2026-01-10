e450ff6dcd0e9e4a949a5adfcfc3474037f20151
lucas <lucaspereirasantos8126@gmail.com>
2026-01-10 12:55:16 +0000

    - Alterações no script de tradução para a transição entre LibreTranslate para Ollama (Phi-3)

--- src/foreign/translate.ts
+++ src/foreign/translate.ts
@@ -1,19 +1,20 @@
-import {decode} from "html-entities";
+import {TranslatedTrivia} from "@/src/types/TranslatedTrivia";
 
-// docker run -ti --rm -p 5000:5000 --env "LT_LOAD_ONLY=en,pt,es" libretranslate/libretranslate
+// docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+// docker exec -it ollama ollama pull phi3
 
-export default async function translate(text: string, output: string) {
-    const res = await fetch("http://127.0.0.1:5000/translate", {
+export default async function translate(prompt: string) {
+    const res = await fetch("http://127.0.0.1:11434/api/generate", {
         method: "POST",
         body: JSON.stringify({
-            q: decode(text),
-            source: "en",
-            target: output,
-            format: "text",
+            model: "phi3",
+            prompt: prompt,
+            format: "json",
+            stream: false,
         }),
         headers: {
             "Content-Type": "application/json"
         }
     });
-    return await res.json();
+    return await res.json() as TranslatedTrivia;
 }
--- src/foreign/uploadSupabase.ts
+++ src/foreign/uploadSupabase.ts
@@ -3,7 +3,7 @@ import {TranslatedTrivia} from "@/src/types/TranslatedTrivia";
 
 export default async function uploadSupabase(questions: TranslatedTrivia[]) {
     const { error } = await supabase_client
-        .from('question_translation_bank')
+        .from('question_translation_bank_test')
         .insert(questions);
     if(error) {throw error;}
 }
